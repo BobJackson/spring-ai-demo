f3bfc96749d029365c96d1ad1792353fa7e93371
Bob Jackson <wangyousong@9191offer.com>
2024-06-12 09:41:29 +0800

    try different model

--- src/main/java/com/example/springaidemo/HelloController.java
+++ src/main/java/com/example/springaidemo/HelloController.java
@@ -1,13 +1,16 @@
 package com.example.springaidemo;
 
 import org.springframework.ai.ollama.api.OllamaApi;
-import org.springframework.ai.ollama.api.OllamaModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${spring.ai.ollama.chat.model}")
+    private String model;
 
     private final OllamaApi api;
 
@@ -19,7 +22,7 @@ public class HelloController {
     public OllamaApi.GenerateResponse embed(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         OllamaApi.GenerateRequest request = OllamaApi.GenerateRequest.builder(message)
                 .withStream(false)
-                .withModel(OllamaModel.LLAMA3.getModelName())
+                .withModel(model)
                 .build();
         return api.generate(request);
     }
